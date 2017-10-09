ee677aca5c76171d614c6fb81c72460f087aed7c
Noah Tanner <NTanner@student.neumont.edu>
2017-10-09 09:07:51 -0600

    Create Board class with it's specifications in the Models folder. Create the GameOfNim class in the GameProcesses folder

---
+++ GameOfNim/GameOfNim/GameProcesses/NimGame.cs
@@ -0,0 +1,30 @@
+﻿using GameOfNim.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfNim.GameProcesses
+{
+    class NimGame
+    {
+        //Player player1;
+        //Player player2;
+        Board gameBoard;
+        int turnCounter;
+        int difficulty;
+        bool isPVP;
+        public NimGame(int difficulty, bool isPVP)
+        {
+            gameBoard = new Board(difficulty);
+
+            if (!isPVP)
+            {
+                //player1 = new Player(true);
+                //Player1.InitializePlayer();
+                //player2 = new Player(false);
+            }
+        }
+    }
+}
